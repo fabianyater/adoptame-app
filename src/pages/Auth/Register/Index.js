ae1262a1d30460290d3680f85fc587d36ecc313d
Fabian Yate <fabianyate31@gmail.com>
2022-10-24 11:42:51 -0500

    fdgfd

--- src/pages/Auth/Register/Index.js
+++ src/pages/Auth/Register/Index.js
@@ -23,22 +23,22 @@ const Register = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='form'>
       <div className='groups' >
-        <div className='input-groups'>
+        <div className='input-group'>
           <label htmlFor='nombre'>Nombre </label>
           <input type='text' id='nombre'{...register("nombre", { required: true })} />
           {errors.nombre && <span className='error' >Este campo es requerido</span>}
         </div>
-        <div className='input-groups'>
+        <div className='input-group'>
           <label htmlFor='apellido'>Apellido </label>
           <input type='text' id='apellido'{...register("apellido", { required: true })} />
           {errors.apellido && <span className='error' >Este campo es requerido</span>}
         </div>
-        <div className='input-groups'>
+        <div className='input-group'>
           <label htmlFor='correo'>Correo </label>
           <input type='email' id='correo'{...register("correo", { required: true, pattern: { patternEmail, message: 'Correo inválido' } })} />
           {errors.correo && <span className='error' >Este campo es requerido</span>}
         </div>
-        <div className='input-groups'>
+        <div className='input-group'>
           <label htmlFor='contrasenia'>Contraseña </label>
           <input type='password' id='contrasenia'{...register("contrasenia", { required: true })} />
           {errors.contrasenia && <span className='error' >Este campo es requerido</span>}
